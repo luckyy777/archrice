d5271cd965a137639cc25934d0a73969ad27cdca
luckyy777 <76631426+luckyy777@users.noreply.github.com>
2022-03-21 15:42:33 +0000

    Add files via upload

--- .local/bin/statusbar/battery.sh
+++ .local/bin/statusbar/battery.sh
@@ -2,7 +2,7 @@
 status="$(cat /sys/class/power_supply/BAT0/status)"
 
 if [ "$status" = "Charging" ]; then
-	echo " $(cat /sys/class/power_supply/BAT0/capacity)% "
+	echo " $(cat /sys/class/power_supply/BAT0/capacity)%"
 else
 	echo " $(cat /sys/class/power_supply/BAT0/capacity)%"
 fi
